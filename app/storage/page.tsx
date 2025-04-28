731574b95c1149629bb290689dc3ad89eca8fbae
sanyam <sanyam.soni27@gmail.com>
2025-04-28 17:32:38 +0530

    upload file

--- app/storage/page.tsx
+++ app/storage/page.tsx
@@ -2,26 +2,37 @@
 
 import React from "react";
 import { uploadData } from "aws-amplify/storage";
-import { FileUploader } from '@aws-amplify/ui-react-storage';
-import '@aws-amplify/ui-react/styles.css';
+import { FileUploader } from "@aws-amplify/ui-react-storage";
+import "@aws-amplify/ui-react/styles.css";
 
 const storage = () => {
-  const [file, setFile] = React.useState();
+  const [file, setFile] = React.useState<File | null>(null);
 
   const handleChange = (event: any) => {
     setFile(event.target.files?.[0]);
   };
 
-  const handleClick = () => {
+  const handleClick = async () => {
     try {
       if (!file) {
         return;
       }
-      uploadData({
-        path: `picture-submissions/${file}`,
+      const result = await uploadData({
+        path: `picture-submissions/${file.name}`,
         data: file,
-      });
-      console.log("File uploaded successfully");
+        options: {
+          onProgress: ({ transferredBytes, totalBytes }) => {
+            if (totalBytes) {
+              console.log(
+                `Upload progress ${Math.round(
+                  (transferredBytes / totalBytes) * 100
+                )} %`
+              );
+            }
+          },
+        },
+      }).result;
+      console.log("File uploaded successfully", result.path);
     } catch (error) {
       console.log("Error uploading file: ", error);
     }
@@ -32,11 +43,11 @@ const storage = () => {
       <input type="file" onChange={handleChange} />
       <button onClick={handleClick}>picture-submissions</button>
       <FileUploader
-      acceptedFileTypes={['image/*']}
-      path="public/"
-      maxFileCount={1}
-      isResumable
-    />
+        acceptedFileTypes={["image/*"]}
+        path="public/"
+        maxFileCount={1}
+        isResumable
+      />
     </div>
   );
 };
